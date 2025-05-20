4b96d10bb1d2926c6d8a1596056caa48a0e027e6
seniordev1220 <157680492+seniordev1220@users.noreply.github.com>
2025-05-20 15:21:30 -0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -58,8 +58,8 @@ export const authOptions = {
       }
     }),
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   pages: {
