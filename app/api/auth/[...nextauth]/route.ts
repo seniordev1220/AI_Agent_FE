ecc927ba2a515377f8a357d38ecbbf1d07a4753f
seniordev1220 <157680492+seniordev1220@users.noreply.github.com>
2025-05-20 14:23:58 -0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,11 +2,11 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
 
-console.log("GOOGLE_CLIENT_ID", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET", process.env.GOOGLE_CLIENT_SECRET);
-console.log("NEXTAUTH_SECRET", process.env.NEXTAUTH_SECRET);
-console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
-console.log("OPENAI_API_KEY", process.env.OPENAI_API_KEY);
+console.log("GOOGLE_CLIENT_ID", process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET", process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET);
+console.log("NEXTAUTH_SECRET", process.env.NEXT_PUBLIC_NEXTAUTH_SECRET);
+console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_NEXTAUTH_URL);
+console.log("OPENAI_API_KEY", process.env.NEXT_PUBLIC_OPENAI_API_KEY);
 
 export const authOptions = {
   providers: [
